de20f34e1d47dcc08618f606bea8a87a3dd0537c
elaiel <mcmartigan@gmx.de>
2017-03-16 13:16:20 +0100

    Update script.js

--- script.js
+++ script.js
@@ -48,10 +48,11 @@ module.exports = new Script({
     
     LebenslaufOptions: {          
         prompt: (bot) => bot.say('%[Berufliche Laufbahn](postback:berufe) %[Akademische Laufbahn](postback:akademia) %[Skills](postback:skills)  %[Sprachen](postback:sprachen)  %[Projekte](postback:projekte)'),      
-          receive: (bot, message) => {
+        receive: (bot, message) => {
             const llselection = message.text;
             switch (llselection) {
                 case "berufe":
+                    return bot.setProp('llselection', llselection)
                     return bot.say('Auswahl: Berufliche Laufbahn?')
                         .then(() => 'berufeOptions');
                 case "akademia":
